afc40216af8303f2e367178fb0cb5141dc0b18e8
Pavlos Panteliadis <pavlospanteliadis@Pavloss-MacBook-Pro.local>
2018-10-06 18:49:18 +0200

    Added a GetHashCode because I was getting an alert

--- Uithoflijn/Station.cs
+++ Uithoflijn/Station.cs
@@ -30,10 +30,18 @@ namespace Uithoflijn
             return Id == c.Id;
         }
 
+        // There was an alert about this on my computer, so I added it. P
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, Name);
+        }
+
         public int GetEmbarkingPassengers(Tram tram, int time)
         {
             //TODO:
             return (int)(WaitingPeople * 0.1);
         }
+
+
     }
 }
